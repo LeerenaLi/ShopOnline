81a2e10361350856e75fd3cb21ea9b3c89363bdf
Leerena <leerena7@gmail.com>
2023-06-23 09:58:49 +0300

    fix

--- src/js/menu.js
+++
@@ -1,7 +0,0 @@
-const menuBtn = document.querySelector('.header__btn');
-const menu = document.querySelector('.navigation');
-
-menuBtn.addEventListener('click', ({target}) => {
-    menuBtn.classList.toggle('header__btn_active');
-    menu.classList.toggle('navigation_active');
-});
