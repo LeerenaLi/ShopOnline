268057ef207b539048e3f09ff6ffaef97bc9075e
Leerena <leerena7@gmail.com>
2023-06-24 21:00:18 +0300

    add cart-page

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,8 @@ import gulpwebp from 'gulp-webp';
 import gulpavif from 'gulp-avif';
 import {stream as critical} from 'critical';
 import gulpif from 'gulp-if';
+import autoprefixer from 'gulp-autoprefixer';
+import babel from 'gulp-babel';
 
 const prepros = true;
 
@@ -43,6 +45,7 @@ export const style = () => {
                 .src('src/scss/**/*.scss')
                 .pipe(gulpif(dev, sourcemaps.init()))
                 .pipe(sass().on('error', sass.logError))
+                .pipe(autoprefixer())
                 .pipe(cleanCss({
                     2: {
                         specialComments: 0,
@@ -60,6 +63,7 @@ export const style = () => {
             .pipe(gulpCssimport({
                 extensions: ['css'],
             }))
+            .pipe(autoprefixer())
             .pipe(cleanCss({
                 2: {
                     specialComments: 0,
@@ -73,6 +77,10 @@ export const style = () => {
 export const js = () => gulp
         .src([...allJS, 'src/js/**/*.js'])
         .pipe(gulpif(dev, sourcemaps.init()))
+        .pipe(babel({
+            presets: ['@babel/preset-env'],
+            ignore: [...allJS, 'src/js/**/*.min.js'],
+        }))
         .pipe(terser())
         .pipe(concat('index.min.js'))
         .pipe(gulpif(dev, sourcemaps.write('../maps')))
@@ -130,7 +138,7 @@ export const critCSS = () => gulp
             inline: true,
             css: ['dist/css/index.min.css'],
         }))
-        .on('eroor', err => {
+        .on('error', err => {
             console.log(err.message);
         })
         .pipe(gulp.dest('dist'));
